230384d592acd3d4d9eb1465205f1a4c80376a3f
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2023-01-06 20:52:07 +0100

    factoring CommonFonts enum

---
+++ tunable/CommonFonts.java
@@ -0,0 +1,36 @@
+package tunable;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.plaf.FontUIResource;
+
+
+public enum CommonFonts {
+  TEXT_NORMAL ("assets" + File.separator + "InterRegular.ttf"),
+  TEXT_MEDIUM_WEIGHT ("assets" + File.separator + "InterSemibold.ttf");
+
+  private Font font;
+
+  private CommonFonts(String fontPath) {
+    try {
+      this.font = Font.createFont(
+        Font.TRUETYPE_FONT,
+        new File(fontPath)
+      );
+    }
+    catch (IOException | FontFormatException e) {
+      System.err.println(e.getMessage());
+
+      this.font = new Font(
+        "helvetica",
+        Font.PLAIN,
+        50
+      );
+    }
+  }
+
+  public Font getFont() {
+    return font;
+  }
+}
