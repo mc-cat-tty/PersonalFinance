c551e68aaba41b539577954138eba6e83b39d7b9
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2023-01-10 02:10:22 +0100

    customize scrollbar

--- ui/components/buttons/FlatScrollBar.java
+++ ui/components/buttons/FlatScrollBar.java
@@ -1,8 +1,10 @@
 package ui.components.buttons;
 
 import javax.swing.JScrollBar;
+import javax.swing.plaf.ColorUIResource;
 import javax.swing.plaf.basic.BasicScrollBarUI;
 
+import tunable.CommonColors;
 import ui.core.IComponent;
 
 import java.awt.*;
@@ -11,7 +13,16 @@ import javax.swing.*;
 public class FlatScrollBar extends JScrollBar implements IComponent {
   public FlatScrollBar() {
     super();
-    
+    UIManager.put("ScrollBar.background", CommonColors.BACKGROUND.getColor());
+    UIManager.put("ScrollBar.thumb", CommonColors.CARD.getColor().brighter());
+    UIManager.put("ScrollBar.thumbDarkShadow", CommonColors.CARD.getColor().brighter());
+    UIManager.put("ScrollBar.thumbHighlight", CommonColors.CARD.getColor().brighter());
+    UIManager.put("ScrollBar.thumbShadow", CommonColors.CARD.getColor().brighter());
+    UIManager.put("ScrollBar.track", CommonColors.CARD.getColor());
+    UIManager.put("ScrollBar.trackHighlight", CommonColors.CARD.getColor());
+
+    setUI(new BasicScrollBarUI());
+
     for (var c : getComponents()) {
       if (c instanceof AbstractButton) {
         remove(c);  // removing increase and decrease button
